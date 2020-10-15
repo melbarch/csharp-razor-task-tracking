eb13949d8cbb1b42316549859e2e91e8abf8d8fd
melbarch <elbarchany@gmail.com>
2020-10-15 21:23:22 +0100

    module 5

--- Tasker/Task.cs
+++ Tasker/Task.cs
@@ -1,10 +1,17 @@
-﻿namespace Tasker
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tasker
 {
     public class Task
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
+        [Required]
+        [Range(1,5)]
         public int Priority { get; set; }
     }
 }
